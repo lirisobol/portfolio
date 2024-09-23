bbba05a38637c9c7349bff1f8f1f54621198f7d6
Liri <lirisobol16@gmail.com>
2024-09-23 14:52:00 +0300

    main title change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,8 @@ export const Home = ():JSX.Element => {
             {/* Hero */}
             <div className="h-full flex flex-col items-center px-10 py-10 sm:py-20 md:py-30">
                 <div className="flex flex-col">
-                    <h3 className="font-bold  text-white text-2xl sm:text-4xl md:text-6xl lg:text-8xl">Liri Sobol</h3>
-                    <p className="font-light text-white text-xl sm:text-2xl md:text-4xl lg:text-6xl">
+                    <h3 className="font-bold text-white text-8xl  ">Liri Sobol</h3>
+                    <p className="font-light text-white text-6xl ">
                         Full Stack Developer
                         <span className="text-4xl font-bold text-teal-200 animate-pulse"> .</span>
                     </p>
