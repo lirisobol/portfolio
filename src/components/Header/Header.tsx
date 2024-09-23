7f0efce9c780eab0f195436324945efb62d7e179
Liri <lirisobol16@gmail.com>
2024-09-23 14:24:43 +0300

    header responsiveness fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = ():JSX.Element => {
     return (
         <div className="
             w-full flex items-center justify-between
-            py-3 px-2 sm:px-4
+            py-4 px-2 sm:px-10
             ">
             <Avatar color="white"/>
             <div>
